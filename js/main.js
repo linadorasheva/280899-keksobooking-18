096c67bc72bd5b63be7c4c069f922369e6b0962a
angelina.dorasheva <dorasheva.a@gmail.com>
2019-11-12 17:00:52 +0300

    ДОбавляет отрисовку пинов-меток на карту

--- js/main.js
+++ js/main.js
@@ -1,38 +1,41 @@
 'use strict';
+var QUANTITY_OBJECTS = 8;
 
-var PhotoUrlMock = {
-  min: '01',
-  max: '08'
+var CoordXMock = {
+  MIN: 350,
+  MAX: 1200
 };
-
 var CoordYMock = {
-  min: 130,
-  max: 630
+  MIN: 130,
+  MAX: 630
 };
-
 var PriceMock = {
-  min: 1000,
-  max: 100000
+  MIN: 1000,
+  MAX: 100000
 };
-
-var typesObjectMock = ['palace','flat','house','bungalo'];
-
 var RoomMock = {
-  min: 1,
-  max: 10
+  MIN: 1,
+  MAX: 10
 };
-
 var GuestsMock = {
-  min: 1,
-  max: 10
+  MIN: 1,
+  MAX: 10
 };
 
-var checkinsMock = ['12:00','13:00','14:00'];
-var checkoutsMock = ['12:00','13:00','14:00'];
-var featuresMock = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var photosMock = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+var pinSize = {
+  width: 50,
+  heigth: 70
+};
 
-
+var typesObjectMock = ['palace', 'flat', 'house', 'bungalo'];
+var checkinsMock = ['12:00', '13:00', '14:00'];
+var checkoutsMock = ['12:00', '13:00', '14:00'];
+var featuresMock = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var photosMock = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var mapBlock = document.querySelector('.map');
+mapBlock.classList.remove('map--faded');
+var pinTemplate = document.querySelector('#pin').content.querySelector('button');
+var pinContainer = document.querySelector('.map__pins');
 var getRandomArrElement = function (arr) {
   var randomArrElement = Math.floor(Math.random() * arr.length);
 
@@ -40,10 +43,64 @@ var getRandomArrElement = function (arr) {
 };
 
 var getRandomInteger = function (min, max) {
-  var randomInteger = min - 0.5 + Math.random() * (max - min + 1);
-  randomInteger = Math.round(randomInteger);
-  return randomInteger;
+  var RandomInteger = min - 0.5 + Math.random() * (max - min + 1);
+  RandomInteger = Math.round(RandomInteger);
+  return RandomInteger;
 };
 
-var mapBlock = document.querySelector('.map');
-mapBlock.classList.remove('map--faded');
+// Одно объявление
+var createAdObject = function (index) {
+  var ad = {
+    'author': {
+      'avatar': 'img/avatars/user0' + index + '.png'
+    },
+    'offer': {
+      'title': 'Заголовок предложения',
+      'address': getRandomInteger(CoordXMock.MIN, CoordXMock.MAX) + ',' + getRandomInteger(CoordYMock.MIN, CoordYMock.MAX),
+      'price': getRandomInteger(PriceMock.MIN, PriceMock.MAX),
+      'type': typesObjectMock[getRandomArrElement(typesObjectMock)],
+      'rooms': getRandomInteger(RoomMock.MIN, RoomMock.MAX),
+      'guests': getRandomInteger(GuestsMock.MIN, GuestsMock.MAX),
+      'checkin': checkinsMock[getRandomArrElement(checkinsMock)],
+      'checkout': checkoutsMock[getRandomArrElement(checkoutsMock)],
+      'features': featuresMock.slice(getRandomInteger(0, featuresMock.length)),
+      'descriptions': 'Описание',
+      'photos': photosMock.slice(getRandomInteger(0, photosMock.length))
+    },
+    'location': {
+      'x': getRandomInteger(CoordXMock.MIN, CoordXMock.MAX),
+      'y': getRandomInteger(CoordYMock.MIN, CoordYMock.MAX)
+    }
+  };
+  return ad;
+};
+// Массив объявлений
+var getArrayAdds = function () {
+  var arrayAdds = [];
+  for (var i = 1; i <= QUANTITY_OBJECTS; i++) {
+    arrayAdds.push(createAdObject(i));
+  }
+  return arrayAdds;
+};
+
+// Метка
+var createElement = function (object) {
+  var element = pinTemplate.cloneNode(true);
+  element.querySelector('img').src = object.author.avatar;
+  element.querySelector('img').alt = object.offer.title;
+  element.style = 'left: ' + (object.location.x - pinSize.width / 2) + 'px;' + 'top: ' + (object.location.y - pinSize.heigth) + 'px;';
+
+  return element;
+};
+
+// Отрисовка меток
+var renderPins = function (array) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(createElement(array[i]));
+  }
+  pinContainer.appendChild(fragment);
+};
+
+var adds = getArrayAdds();
+renderPins(adds);
